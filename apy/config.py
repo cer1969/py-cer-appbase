6fd9f7a03f1dffc13126ef36c252f5140b27aacd
Cristian Echeverría <cer1969@gmail.com>
2009-12-17 15:48:43 -0300

    Cambio para adaptar estilo de c?digo

--- apy/config.py
+++ apy/config.py
@@ -15,9 +15,9 @@ version = "0.1.0"
 # User options
 _x, _y, _w, _h = wx.GetClientDisplayRect()
 ini = AppIni(cerapp.toAppDir("data/%s.ini" % name))
-ini.add_section("app")
-ini.add_int("app.width",  900, vmin=600, vmax=_w)
-ini.add_int("app.height", 700, vmin=400, vmax=_h)
+ini.addSection("app")
+ini.addInt("app.width",  900, vmin=600, vmax=_w)
+ini.addInt("app.height", 700, vmin=400, vmax=_h)
 ini.load(create=True)
 
 # Application information
